a222d70a682f16df38bf48a80e8f7d03d326cde4
EnricoSailis <149102018+EnricoSailis@users.noreply.github.com>
2024-08-07 18:34:23 +0200

    Add files via upload

---
+++ DaEsadecimaleAIntero.py
@@ -0,0 +1,6 @@
+# Trasforma un numero esadecimale in un numero intero
+n = input("Inserisci un numero esadecimale da convertire in numero intero: ")
+hex_string = n
+integer_value = int(hex_string,16)
+print('Intero:')
+print(integer_value)
---
+++ DaNumeroAEsadecimale.py
@@ -0,0 +1,6 @@
+# Trasforma un numero intero in una stringa esadecimale 
+n = int(input("Inserisci un numero intero da convertire in esadecimale: "))
+integer_value = n
+hex_string = hex(integer_value)
+print('testo:')
+print(hex_string)
