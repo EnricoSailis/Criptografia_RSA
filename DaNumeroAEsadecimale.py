7bfabae943d98772782fe5db5526276cbe7a86c4
EnricoSailis <149102018+EnricoSailis@users.noreply.github.com>
2024-08-11 20:32:21 +0200

    Delete DaNumeroAEsadecimale.py

--- DaNumeroAEsadecimale.py
+++
@@ -1,6 +0,0 @@
-# Trasforma un numero intero in una stringa esadecimale 
-n = int(input("Inserisci un numero intero da convertire in esadecimale: "))
-integer_value = n
-hex_string = hex(integer_value)
-print('testo:')
-print(hex_string)
